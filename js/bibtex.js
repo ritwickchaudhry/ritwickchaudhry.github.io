a846c9fc7338478c7729653cd24ea2430d5b224c
ritwic <ritwic@amazon.com>
2021-10-07 16:15:02 -0700

    Adding first version

---
+++ js/bibtex.js
@@ -0,0 +1,71 @@
+function sg() {
+    var div = document.getElementById('sg_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function tracking() {
+    var div = document.getElementById('tracking_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function leafqa() {
+    var div = document.getElementById('leafqa_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function tomography() {
+    var div = document.getElementById('tomography_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function icdar() {
+    var div = document.getElementById('icdar_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function colearn() {
+    var div = document.getElementById('colearn_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function videomem() {
+    var div = document.getElementById('videomem_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
+
+function dicta() {
+    var div = document.getElementById('dicta_bib');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'block';
+    }
+}
---
+++ js/shuffle.js
@@ -0,0 +1,11 @@
+function shuffle_amazon() {
+    shuffleLetters(document.getElementById('amazon_email'), {
+        text: 'ritwic@amazon.com',
+    });
+}
+
+function shuffle_personal() {
+    shuffleLetters(document.getElementById('personal_email'), {
+        text: 'ritwickchaudhry@gmail.com',
+    });
+}
